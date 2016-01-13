f15e534af091b9ec61ad11a5634872a2654a319e
jacksonasinmichael <league@Mac-mini.local>
2016-01-13 14:51:42 -0800

    1-13-16
    We did a New pentagon crazy

---
+++ src/BeiberFever.java
@@ -0,0 +1,19 @@
+import javax.swing.JOptionPane;
+
+public class BeiberFever {
+	public static void main(String[] args) {
+		for (int i = 0; i < 3; i++) {
+			for (int j = 0; j < 3; j++) {
+				System.out.println("baby");
+			}
+			if (i == 1) {
+				System.out.println("no");
+			} else {
+				System.out.println("oh");
+			}
+
+		}
+		System.out.println("thought you'd always be mine");
+	}
+	
+}
