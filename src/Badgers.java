3dab7fd3fe4697cb4feef4101374e24906a775ed
OrigamiMM <league@mini222.local>
2016-03-09 14:54:48 -0800

    3-09-16
    Everything we did today

--- src/Badgers.java
+++ src/Badgers.java
@@ -1,11 +1,26 @@
 public class Badgers {
 	public static void main(String[] args) {
-		for (int i = 0; i < 14; i++) {
-			System.out.println("Badgers");
-			if (i == 12) {
-			System.out.println("mushroom" + "\n" + "Mushroom");
+		for (int i = 0; i < 2; i++) {
+			for (int d = 1; d < 13; d++) {
+				System.out.println("Badgers");
+				speak("Badgers");
+
+				if (d == 12) {
+					System.out.println("Mushroom" + "\n" + "Mushroom");
+					speak("Mushroom Mushroom");
+				}
+
+			}
 		}
-		
+		System.out.println("snake, oh a snake");
+		speak("snake, oh a snake");
+	}
+
+	static void speak(String words) {
+		try {
+			Runtime.getRuntime().exec("say " + words).waitFor();
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
 	}
 }
