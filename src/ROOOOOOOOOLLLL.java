551bb252339e4fdd5fa9861581a2c0c8c7f52e3d
OrigamiMM <league@Mac-mini.local>
2017-01-05 16:55:23 -0800

    1-05-17
    wcvyjgsdvjygde

--- src/ROOOOOOOOOLLLL.java
+++ src/ROOOOOOOOOLLLL.java
@@ -1,4 +1,33 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Random;
 
-public class ROOOOOOOOOLLLL {
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class ROOOOOOOOOLLLL implements ActionListener {
+	JFrame frame = new JFrame();
+	JPanel pan =  new JPanel();
+	JButton but = new JButton("Roll Dice");
+	int number = null;
+public static void main(String[] args) {
+	ROOOOOOOOOLLLL g = new ROOOOOOOOOLLLL();
+    g.setup();
+    int number = new Random().nextInt(6);
+}
+void setup(){
+	frame.add(pan);
+	pan.add(but);
+	frame.pack();
+	but.addActionListener(this);
+	
+	
+}
+@Override
+public void actionPerformed(ActionEvent e) {
+	// TODO Auto-generated method stub
+	
+}
 
 }
